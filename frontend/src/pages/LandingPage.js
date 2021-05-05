4975264ebb5379cb66d60adf26b97f5d899b6c36
suwidnyana <grayfullbuster522@gmail.com>
2021-05-05 09:25:18 +0800

    finishing section categories

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -4,6 +4,7 @@ import Header from 'parts/Header';
 import Hero from 'parts/Hero';
 import landingPage from 'json/landingPage.json';
 import MostPicked from 'parts/MostPicked';
+import Categories from 'parts/Categories'
 export default class LandingPage extends Component{
 
     constructor(props){
@@ -16,8 +17,11 @@ export default class LandingPage extends Component{
         <>
             <Header {...this.props}></Header>
             <Hero refMostPicked={this.refMostPicked}  data={landingPage.hero}/>
-            <MostPicked  refMostPicked={this.refMostPicked} data={landingPage.mostPicked}/>
-
+            <MostPicked  
+                refMostPicked={this.refMostPicked} 
+                data={landingPage.mostPicked}
+            />
+            <Categories data={landingPage.categories}/>
         </>)
     }
 }
