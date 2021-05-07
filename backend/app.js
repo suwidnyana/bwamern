ac7784397ba005d5173b69591b5ba8150159944c
suwidnyana <grayfullbuster522@gmail.com>
2021-05-07 22:51:36 +0800

    add view dashboard js for backend

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const adminRouter = require('./routes/admin')
+
 var app = express();
 
 // view engine setup
@@ -22,6 +24,10 @@ app.use('/sb-admin-2', express.static(path.join(__dirname, 'node_modules/startbo
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use('/admin', adminRouter)
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
