c3755e40f75bd863efefc4bb8a66cfd7ed484c4f
suwidnyana <grayfullbuster522@gmail.com>
2021-07-16 14:39:26 +0800

    crud category delete section

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -22,6 +22,13 @@ module.exports = {
         await category.save();
         res.redirect('/admin/category')
     },
+
+    deleteCategory: async(req, res) => {
+        const {id} = req.params;
+        const category = await Category.findOne({_id: id});
+        await category.remove();
+        res.redirect('/admin/category');
+    },
     viewBank: (req, res) => {
         res.render('admin/bank/view_bank');
     },
--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -6,7 +6,7 @@ router.get('/dashboard', adminController.viewDashboard)
 router.get('/category', adminController.viewCategory)
 router.post('/category', adminController.addCategory)
 router.put('/category', adminController.editCategory)
-
+router.delete('/category/:id', adminController.deleteCategory);
 router.get('/bank', adminController.viewBank)
 router.get('/item', adminController.viewItem)
 router.get('/booking', adminController.viewBooking)
