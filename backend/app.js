8df1cd143ed027cbc8cce6012f5d6558b34f7ee8
suwidnyana <grayfullbuster522@gmail.com>
2021-07-21 12:51:57 +0800

    add session and alert message

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,21 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const methodOverride = require('method-override')
-
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://root:root@cluster0.ducuv.mongodb.net/db_staycation?retryWrites=true&w=majority',{
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const methodOverride = require("method-override");
+const session = require("express-session");
+const flash = require("connect-flash");
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb+srv://root:root@cluster0.ducuv.mongodb.net/db_staycation?retryWrites=true&w=majority",
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
     useCreateIndex: true,
-})
-
-
-
+  }
+);
 
 // const CONNECTION_URL = 'mongodb+srv://root:root@cluster0.ducuv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 // const PORT = process.env.PORT|| 5000;
@@ -25,51 +26,56 @@ mongoose.connect('mongodb+srv://root:root@cluster0.ducuv.mongodb.net/db_staycati
 
 // mongoose.set('useFindAndModify', false);
 
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-
-
-
-const adminRouter = require('./routes/admin')
+const adminRouter = require("./routes/admin");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
-app.use(methodOverride('_method'));
+app.use(logger("dev"));
+app.use(methodOverride("_method"));
+app.use(
+  session({
+    secret: "secretkey",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 6000 },
+  })
+);
+app.use(flash());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/sb-admin-2', express.static(path.join(__dirname, 'node_modules/startbootstrap-sb-admin-2')))
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  "/sb-admin-2",
+  express.static(path.join(__dirname, "node_modules/startbootstrap-sb-admin-2"))
+);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
-app.use('/admin', adminRouter)
-
-
+app.use("/admin", adminRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
