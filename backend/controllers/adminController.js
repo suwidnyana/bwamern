908dc9ae1bbe303cf770cd5188d615d0883781b2
suwidnyana <grayfullbuster522@gmail.com>
2021-08-06 17:55:44 +0800

    add update bank and delete

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,5 +1,7 @@
 const Category = require("../models/Category");
 const Bank = require("../models/Bank");
+const fs = require("fs-extra");
+const path = require("path");
 module.exports = {
   viewDashboard: (req, res) => {
     res.render("admin/dashboard/view_dashboard", {
@@ -94,6 +96,52 @@ module.exports = {
         imageUrl: `images/${req.file.filename}`,
       });
       req.flash("alertMessage", "Success Add Bank");
+      req.flash("alertStatus", "success");
+      res.redirect("/admin/bank");
+    } catch (error) {
+      req.flash("alertMessage", `${error.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/admin/bank");
+    }
+  },
+
+  editBank: async (req, res) => {
+    try {
+      const { id, name, nameBank, nomorRekening } = req.body;
+      const bank = await Bank.findOne({ _id: id });
+      if (req.file == undefined) {
+        bank.name = name;
+        bank.nameBank = nameBank;
+        bank.nomorRekening = nomorRekening;
+        await bank.save();
+        req.flash('alertMessage', 'Success Update Bank');
+        req.flash('alertStatus', 'success');
+        res.redirect('/admin/bank');
+      } else {
+        await fs.unlink(path.join(`public/${bank.imageUrl}`));
+        bank.name = name;
+        bank.nameBank = nameBank;
+        bank.nomorRekening = nomorRekening;
+        bank.imageUrl = `images/${req.file.filename}`
+        await bank.save();
+        req.flash('alertMessage', 'Success Update Bank');
+        req.flash('alertStatus', 'success');
+        res.redirect('/admin/bank');
+      }
+    } catch (error) {
+      console.log(error);
+      req.flash("alertMessage", `${error.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/admin/bank");
+    }
+  },
+  deleteBank: async(req, res) => {
+    try {
+      const { id } = req.params;
+      const bank = await Bank.findOne({ _id: id });
+      await fs.unlink(path.join(`public/${bank.imageUrl}`));
+      await bank.remove();
+      req.flash("alertMessage", "Success Delete Bank");
       req.flash("alertStatus", "success");
       res.redirect("/admin/bank");
     } catch (error) {
--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -10,6 +10,8 @@ router.put("/category", adminController.editCategory);
 router.delete("/category/:id", adminController.deleteCategory);
 router.get("/bank", adminController.viewBank);
 router.post("/bank", upload, adminController.addBank);
+router.put("/bank",upload, adminController.editBank);
+router.delete("/bank/:id", adminController.deleteBank);
 router.get("/item", adminController.viewItem);
 router.get("/booking", adminController.viewBooking);
 
