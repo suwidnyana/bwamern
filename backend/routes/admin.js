accfc348d082ec04c33fde49980b78b90a6008cf
suwidnyana <grayfullbuster522@gmail.com>
2021-08-08 23:52:03 +0800

    add item ,upload multiple images

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,18 +1,23 @@
 const router = require("express").Router();
 
 const adminController = require("../controllers/adminController");
-const { upload } = require("../middleware/multer");
+const { uploadSingle, uploadMultiple } = require("../middleware/multer");
 
 router.get("/dashboard", adminController.viewDashboard);
 router.get("/category", adminController.viewCategory);
 router.post("/category", adminController.addCategory);
 router.put("/category", adminController.editCategory);
 router.delete("/category/:id", adminController.deleteCategory);
+
 router.get("/bank", adminController.viewBank);
-router.post("/bank", upload, adminController.addBank);
-router.put("/bank",upload, adminController.editBank);
+router.post("/bank", uploadSingle, adminController.addBank);
+router.put("/bank",uploadSingle, adminController.editBank);
 router.delete("/bank/:id", adminController.deleteBank);
+
+
 router.get("/item", adminController.viewItem);
+router.post('/item', uploadMultiple, adminController.addItem);
+
 router.get("/booking", adminController.viewBooking);
 
 module.exports = router;
