e2c31afdac1723fdf917620339d6fe8f029f6419
suwidnyana <grayfullbuster522@gmail.com>
2021-08-12 01:09:54 +0800

    show edit item

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -18,6 +18,7 @@ router.delete("/bank/:id", adminController.deleteBank);
 router.get("/item", adminController.viewItem);
 router.get("/item/show-image/:id", adminController.showImageItem);
 router.post('/item', uploadMultiple, adminController.addItem);
+router.get("/item/:id", adminController.showEditItem);
 
 router.get("/booking", adminController.viewBooking);
 
