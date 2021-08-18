6a220aeacd4226e6b03c209bde2f868e65737093
suwidnyana <grayfullbuster522@gmail.com>
2021-08-18 09:06:33 +0800

    add delete image item

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -20,6 +20,7 @@ router.get("/item/show-image/:id", adminController.showImageItem);
 router.post('/item', uploadMultiple, adminController.addItem);
 router.get("/item/:id", adminController.showEditItem);
 router.put("/item/:id",uploadMultiple, adminController.editItem);
+router.delete('/item/:id/delete', adminController.deleteItem);
 
 router.get("/booking", adminController.viewBooking);
 
