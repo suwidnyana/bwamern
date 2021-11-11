5806219684441b308795f029ce153ce439598786
suwidnyana <grayfullbuster522@gmail.com>
2021-11-11 22:19:08 +0800

    @

--- backend/controllers/apiController.js
+++ backend/controllers/apiController.js
@@ -9,21 +9,40 @@ module.exports = {
             const mostPicked = await Item.find()
             .select('_id title country city price unit imageId')
             .limit(5)
-            .populate({path: 'imageId', select: '_id imageUrl'})
-
-            const traveler = await Traveler.find()
-            const treasure = await Treasure.find()
-            const city = await Item.find()
-            
-            const category =  await Category.find()
+            .populate({ path: 'imageId', select: '_id imageUrl' })
+    
+          const category = await Category.find()
             .select('_id name')
             .limit(3)
             .populate({
-                path: 'itemId',
-                select: '_id title country isPopular imageId',
-                perDocumentLimit: 4,
-                populate: {path: 'imageId', select: '_id imageUrl', perDocumentLimit: 1}
+              path: 'itemId',
+              select: '_id title country city isPopular  imageId',
+              perDocumentLimit: 4,
+              option: { sort: { sumBooking: -1 } },
+              populate: {
+                path: 'imageId',
+                select: '_id imageUrl',
+                perDocumentLimit: 1
+              }
             })
+    
+          const traveler = await Traveler.find();
+          const treasure = await Treasure.find();
+          const city = await Item.find();
+          
+      for (let i = 0; i < category.length; i++) {
+        for (let x = 0; x < category[i].itemId.length; x++) {
+          const item = await Item.findOne({ _id: category[i].itemId[x]._id });
+          item.isPopular = false;
+          await item.save();
+          if (category[i].itemId[0] === category[i].itemId[x]) {
+            item.isPopular = true;
+            await item.save();
+          }
+        }
+      }
+
+          
 
             res.status(200).json({
                 hero: {
@@ -37,6 +56,7 @@ module.exports = {
            
         } catch (error) {
             console.log(error)
+            res.status(500).json({message: "internal server error"})
         }
        
     },
