ea8583ce42dfaace10c57188ad0d658c4ecfbf54
suwidnyana <grayfullbuster522@gmail.com>
2021-12-17 16:52:29 +0800

    remove .env and add .env.example

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,210 +1,210 @@
-import React, { Component } from 'react'
-import Header from 'parts/Header'
-import Fade from 'react-reveal/Fade'
-import Button from 'elements/Button'
-import Stepper, { Numbering, Meta, MainContent, Controller } from 'elements/Stepper'
+import React, { Component } from "react";
+import Header from "parts/Header";
+import Fade from "react-reveal/Fade";
+import Button from "elements/Button";
+import Stepper, {
+  Numbering,
+  Meta,
+  MainContent,
+  Controller,
+} from "elements/Stepper";
 import { connect } from "react-redux";
 
-import BookingInformation from 'parts/Checkout/BookingInformation'
-import Payment from 'parts/Checkout/Payment'
-import Completed from 'parts/Checkout/Completed'
+import BookingInformation from "parts/Checkout/BookingInformation";
+import Payment from "parts/Checkout/Payment";
+import Completed from "parts/Checkout/Completed";
 
-import ItemDetails from "json/itemDetails.json"
+import ItemDetails from "json/itemDetails.json";
 class Checkout extends Component {
-    state = {
-        data: {
-            firstName: "",
-            lastName: "",
-            email: "",
-            phone: "",
-            proofPayment: "",
-            bankName: "",
-            bankHolder: "",
-        },
+  state = {
+    data: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      phone: "",
+      proofPayment: "",
+      bankName: "",
+      bankHolder: "",
+    },
+  };
+
+  onChange = (event) => {
+    this.setState({
+      data: {
+        ...this.state.data,
+        [event.target.name]: event.target.value,
+      },
+    });
+  };
+
+  componentDidMount() {
+    window.scroll(0, 0);
+    document.title = "Staycation | Checkout";
+  }
+
+  render() {
+    const { data } = this.state;
+    const { checkout } = this.props;
+    if (!checkout)
+      return (
+        <div className="container">
+          <div
+            className="row align-items-center justify-content-center text-center"
+            style={{
+              height: "100vh",
+            }}
+          >
+            <div className="col-3">
+              Pilih kamar dulu{" "}
+              <div>
+                <Button
+                  className="btn mt-5"
+                  type="button"
+                  onClick={() => this.props.history.goBack()}
+                  isLight
+                >
+                  Back{" "}
+                </Button>{" "}
+              </div>{" "}
+            </div>{" "}
+          </div>{" "}
+        </div>
+      );
+
+    const steps = {
+      bookingInformation: {
+        title: "Booking information",
+        description: "Please fill up the blank field below",
+        content: (
+          <BookingInformation
+            data={data}
+            checkout={checkout}
+            ItemDetails={ItemDetails}
+            onChange={this.onChange}
+          />
+        ),
+      },
+      payment: {
+        title: "Payment",
+        description: "Kindly follow the instructions below",
+        content: (
+          <Payment
+            data={data}
+            checkout={checkout}
+            ItemDetails={ItemDetails}
+            onChange={this.onChange}
+          />
+        ),
+      },
+      completed: {
+        title: "Yay! Completed",
+        description: null,
+        content: <Completed />,
+      },
     };
 
-
-    onChange = (event) => {
-        this.setState({
-            data: {
-                ...this.state.data,
-                [event.target.name]: event.target.value,
-            },
-        })
-    }
-
-    componentDidMount() {
-        window.scroll(0, 0);
-        document.title = "Staycation | Checkout";
-      }
-
-
-      
-    render() {
-        const { data } = this.state
-        const {checkout} = this.props
-        if (!checkout)
-        return (
-          <div className="container">
-            <div
-              className="row align-items-center justify-content-center text-center"
-              style={{ height: "100vh" }}
-            >
-              <div className="col-3">
-                Pilih kamar dulu
-                <div>
-                  <Button
-                    className="btn mt-5"
-                    type="button"
-                    onClick={() => this.props.history.goBack()}
-                    isLight
-                  >
-                    Back
-                  </Button>
-                </div>
-              </div>
-            </div>
-          </div>
-        );
-  
-        const steps = {
-            bookingInformation: {
-                title: "Booking information",
-                description: "Please fill up the blank field below",
-                content: (
-                    <BookingInformation
-                        data={data}
-                        checkout={checkout}
-                        ItemDetails={ItemDetails}
-                        onChange={this.onChange}
-                    />
-                )
-            },
-            payment: {
-                title: "Payment",
-                description: "Kindly follow the instructions below",
-                content: (
-                    <Payment
-                        data={data}
-                        checkout={checkout}
-                        ItemDetails={ItemDetails}
-                        onChange={this.onChange}
-                    />
-                )
-            },
-            completed: {
-                title: "Yay! Completed",
-                description: null,
-                content: <Completed />
-            }
-
-        }
-
-        return (
+    return (
+      <>
+        <Header isCentered />
+        <Stepper steps={steps}>
+          {" "}
+          {(prevStep, nextStep, CurrentStep, steps) => (
             <>
-                <Header isCentered />
-                <Stepper steps={steps}>
-                    {(prevStep, nextStep, CurrentStep, steps) => (
-                            <>
-                                <Numbering
-                                    data={steps}
-                                    current={CurrentStep}
-                                    style={{ marginBottom: 50 }}
-
-                                />
-                                <Meta data={steps} current={CurrentStep} />
-                                <MainContent data={steps} current={CurrentStep} />
-                               
-                                {CurrentStep === "bookingInformation" && (
-                                    <Controller>
-                                    {data.firstName !== "" &&
-                                      data.lastName !== "" &&
-                                      data.email !== "" &&
-                                      data.phone !== "" && (
-                                        <Fade>
-                                          <Button
-                                            className="btn mb-3"
-                                            type="button"
-                                            isBlock
-                                            isPrimary
-                                            hasShadow
-                                            onClick={nextStep}
-                                          >
-                                            Continue to Book
-                                          </Button>
-                                        </Fade>
-                                      )}
-                                    <Button
-                                      className="btn"
-                                      type="link"
-                                      isBlock
-                                      isLight
-                                     href={`/properties/${ItemDetails._id}`}
-                                    >
-                                      Cancel
-                                    </Button>
-                                  </Controller>
-                                )}
-
-                                {CurrentStep === "payment" && (
-                                    <Controller>
-                                        {data.proofPayment !== "" &&
-                                            data.bankName !== "" &&
-                                            data.bankHolder !== "" && (
-                                                <Fade>
-                                                    <Button
-                                                        className="btn mb-3"
-                                                        type="button"
-                                                        isBlock
-                                                        isPrimary
-                                                        hasShadow
-                                                        onClick={nextStep}
-
-                                                    >
-                                                        Continue to Book
-                                                    </Button>
-                                                </Fade>
-
-                                            )}
-                                        <Button
-                                            className="btn"
-                                            type="link"
-                                            isBlock
-                                            isLight
-                                            onClick={prevStep}
-                                        >
-                                            Cancel
-                                        </Button>
-                                    </Controller>
-                                )}
-
-                                {CurrentStep === "completed" && (
-                                    <Controller>
-                                        <Button
-                                            className="btn"
-                                            type="link"
-                                            isBlock
-                                            isPrimary
-                                            hasShadow
-                                            href=""
-                                        
-                                        >
-                                            Back to Home
-                                        </Button>
-                                    </Controller>
-                                )}
-                            </>
-                        )}
-                </Stepper>
+              <Numbering
+                data={steps}
+                current={CurrentStep}
+                style={{
+                  marginBottom: 50,
+                }}
+              />{" "}
+              <Meta data={steps} current={CurrentStep} />{" "}
+              <MainContent data={steps} current={CurrentStep} />
+              {CurrentStep === "bookingInformation" && (
+                <Controller>
+                  {" "}
+                  {data.firstName !== "" &&
+                    data.lastName !== "" &&
+                    data.email !== "" &&
+                    data.phone !== "" && (
+                      <Fade>
+                        <Button
+                          className="btn mb-3"
+                          type="button"
+                          isBlock
+                          isPrimary
+                          hasShadow
+                          onClick={nextStep}
+                        >
+                          Continue to Book{" "}
+                        </Button>{" "}
+                      </Fade>
+                    )}{" "}
+                  <Button
+                    className="btn"
+                    type="link"
+                    isBlock
+                    isLight
+                    href={`/properties/${ItemDetails._id}`}
+                  >
+                    Cancel{" "}
+                  </Button>{" "}
+                </Controller>
+              )}
+              {CurrentStep === "payment" && (
+                <Controller>
+                  {" "}
+                  {data.proofPayment !== "" &&
+                    data.bankName !== "" &&
+                    data.bankHolder !== "" && (
+                      <Fade>
+                        <Button
+                          className="btn mb-3"
+                          type="button"
+                          isBlock
+                          isPrimary
+                          hasShadow
+                          onClick={nextStep}
+                        >
+                          Continue to Book{" "}
+                        </Button>{" "}
+                      </Fade>
+                    )}{" "}
+                  <Button
+                    className="btn"
+                    type="link"
+                    isBlock
+                    isLight
+                    onClick={prevStep}
+                  >
+                    Cancel{" "}
+                  </Button>{" "}
+                </Controller>
+              )}
+              {CurrentStep === "completed" && (
+                <Controller>
+                  <Button
+                    className="btn"
+                    type="link"
+                    isBlock
+                    isPrimary
+                    hasShadow
+                    href=""
+                  >
+                    Back to Home{" "}
+                  </Button>{" "}
+                </Controller>
+              )}{" "}
             </>
-        )
-    }
+          )}{" "}
+        </Stepper>{" "}
+      </>
+    );
+  }
 }
 
 const mapStateToProps = (state) => ({
-    checkout: state.checkout,
-    page: state.page,
-})
+  checkout: state.checkout,
+  page: state.page,
+});
 
-
-export default connect(mapStateToProps)(Checkout)
+export default connect(mapStateToProps)(Checkout);
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,53 +1,54 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { connect } from "react-redux";
 
-import Header from 'parts/Header';
-import Hero from 'parts/Hero';
+import Header from "parts/Header";
+import Hero from "parts/Hero";
 // import landingPage from 'json/landingPage.json';
-import MostPicked from 'parts/MostPicked';
-import Categories from 'parts/Categories'
-import Testimony from 'parts/Testimony'
-import Footer from 'parts/Footer'
+import MostPicked from "parts/MostPicked";
+import Categories from "parts/Categories";
+import Testimony from "parts/Testimony";
+import Footer from "parts/Footer";
 
-import {fetchPage} from "store/actions/pages"
+import { fetchPage } from "store/actions/pages";
 class LandingPage extends Component {
+  constructor(props) {
+    super(props);
+    this.refMostPicked = React.createRef();
+  }
 
-    constructor(props) {
-        super(props);
-        this.refMostPicked = React.createRef();
-    }
+  componentDidMount() {
+    window.title = "Staycation | Home";
+    window.scrollTo(0, 0);
 
-    componentDidMount() {
-        window.title = "Staycation | Home";
-        window.scrollTo(0, 0);
+    if (!this.props.page.landingPage)
+      this.props.fetchPage(
+        `${process.env.REACT_APP_HOST}/api/v1/member/landing-page`,
+        "landingPage"
+      );
+  }
 
+  render() {
+    const { page } = this.props;
 
-        if(!this.props.page.landingPage) 
-        this.props.fetchPage(`${process.env.REACT_APP_HOST}/api/v1/member/landing-page`, 'landingPage')
-    }
-
-    render() {
-
-        const { page } = this.props
-      
-        if (!page.hasOwnProperty("landingPage")) return null;
-        return (
-            <>
-                <Header {...this.props}></Header>
-                <Hero refMostPicked={this.refMostPicked} data={page.landingPage.hero} />
-                <MostPicked
-                    refMostPicked={this.refMostPicked}
-                    data={page.landingPage.mostPicked}
-                />
-                <Categories data={page.landingPage.category} />
-                <Testimony data={page.landingPage.testimonial} />
-                <Footer />
-            </>)
-    }
+    if (!page.hasOwnProperty("landingPage")) return null;
+    return (
+      <>
+        <Header {...this.props}></Header>
+        <Hero refMostPicked={this.refMostPicked} data={page.landingPage.hero} />
+        <MostPicked
+          refMostPicked={this.refMostPicked}
+          data={page.landingPage.mostPicked}
+        />
+        <Categories data={page.landingPage.category} />
+        <Testimony data={page.landingPage.testimonial} />
+        <Footer />
+      </>
+    );
+  }
 }
 
 const mapStateToProps = (state) => ({
-    page: state.page
-})
+  page: state.page,
+});
 
-export default connect(mapStateToProps, {fetchPage})(LandingPage)
+export default connect(mapStateToProps, { fetchPage })(LandingPage);
